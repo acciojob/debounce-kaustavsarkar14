9ce9ef87cf5b01886bf85601cd0011e9a09c8961
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-03-04 11:24:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function debounce(callback, delay, immediate = false) {
-  
+   let timer;
+
+  return function(...args) {
+    const context = this;
+
+    const later = () => {
+      timer = null;
+      if (!immediate) {
+        callback.apply(context, args);
+      }
+    };
+
+    const callNow = immediate && !timer;
+    
+    clearTimeout(timer);
+    timer = setTimeout(later, delay);
+
+    if (callNow) {
+      callback.apply(context, args);
+    }
+  };
   }
   
   module.exports = debounce;
